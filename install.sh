7b6a71a6b68f6ed9fab7d5fd8640057dc4a1748c
AndreLucas2103 <p4n1c0021@gmail.com>
2024-12-25 00:32:43 -0300

    backup

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ cd /var/www/html
 echo "Instalando pacote java ${JAR_FILENAME}"
 echo "Verificando variável de treinamento ${TRAINING}"
 
-echo "java -jar ${JAR_FILENAME} -console -url="jdbc:postgresql://psql:5432/${POSTGRES_DATABASE}" -username ${POSTGRES_USERNAME} -password ${POSTGRES_PASSWORD} ${TRAINING}"
-java -jar ${JAR_FILENAME} -console -url="jdbc:postgresql://psql:5432/${POSTGRES_DATABASE}" -username ${POSTGRES_USERNAME} -password ${POSTGRES_PASSWORD} ${TRAINING}
+echo "java -jar ${JAR_FILENAME} -console -url="jdbc:postgresql://${POSTGRES_URL_SERVER}:${POSTGRES_PORT}/${POSTGRES_DATABASE}" -username ${POSTGRES_USERNAME} -password ${POSTGRES_PASSWORD} ${TRAINING}"
+java -jar ${JAR_FILENAME} -console -url="jdbc:postgresql://${POSTGRES_URL_SERVER}:${POSTGRES_PORT}/${POSTGRES_DATABASE}" -username ${POSTGRES_USERNAME} -password ${POSTGRES_PASSWORD} ${TRAINING}
